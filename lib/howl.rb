987beaa4b3357f646ce71dd574f761acc4ba4509
namusyaka <namusyaka@gmail.com>
2013-08-22 19:28:39 +0900

    Insert yardoc before method definition, and change accessor definition order.

--- lib/howl.rb
+++ lib/howl.rb
@@ -7,12 +7,9 @@ require 'rack'
   request
 ].each{|name| require File.expand_path("../howl/#{name}", __FILE__) }
 
-
 class Howl
   class InvalidRouteException < ArgumentError
   end
-
-  attr_accessor :request, :response
 
   HTTP_VERBS       = [:get, :post, :delete, :put, :head]
   RESPONSE_HEADERS = {
@@ -22,6 +19,21 @@ class Howl
     :server_error       => 500
   }
 
+  attr_accessor :request, :response
+
+  # Generate a route, and add to routes.
+  #
+  # @param [String, Symbol] verb The verb decide a acceptable request method.
+  # @param [String] path The path associate to route.
+  # @option options [String] :path_for_generation Accept path_for_generation.
+  # @yield The block assosiate to route.
+  #
+  # @example
+  #   howl = Howl.new
+  #   howl.add(:get, "/") #=> Howl::Route
+  #
+  # @return [Howl::Route] Return a generated Howl::Route instance.
+  #
   def add(verb, path, options = {}, &block)
     verb = verb.downcase.to_sym
     (router.routes_with_verbs[verb] ||= []) << (route = Route.new(path, &block))
@@ -32,12 +44,11 @@ class Howl
     route
   end
 
+  # call method for Rack Application.
   def call(env)
     request  = Request.new(env)
     return bad_request unless HTTP_VERBS.include?(request.request_method.downcase.to_sym)
-
     compile unless compiled?
-
     begin
       matched_routes = recognize(request)
       route, params = matched_routes.first
@@ -52,38 +63,76 @@ class Howl
     end
   end
 
+  # Determines whether the compiled.
+  #
+  # @return [Bool]
   def compiled?
     @compiled
   end
 
+  # Compile routes.
+  #
+  # @return [Array] Return a compiled routes.
   def compile
     @compiled = true
     router.compile
   end
 
+  # Recognize a request, and return a matched routes.
+  #
+  # @param [Rack::Request] request The request is a Rack::Request or instance that inherited it.
+  #
+  # @return [Array] Return a routes that match the path_info.
   def recognize(request)
     router.recognize(request)
   end
 
+  # Recognize a path_info, and return a matched first route's name and params.
+  #
+  # @param [String] path_info
+  #
+  # @return [Array] Return a Array that likes [name, params].
   def recognize_path(path_info)
     response      = router.recognize(Rack::MockRequest.env_for(path_info))
     route, params = response.first
     [route.name, params]
   end
 
+  # Reset a router.
   def reset!
     @router = @compiled = nil
     router.reset!
   end
 
+  # Return a Router instance.
+  #
+  # @return [Howl::Router]
   def router
     @router ||= Router.new
   end
 
+  # Return a added routes.
+  #
+  # @return [Array]
   def routes
     router.routes
   end
 
+  # Find a route, and return a generated path of route.
+  #
+  # @param [Symbol] name The name is route name.
+  # @param [Array] args The args are route params and queries.
+  #
+  # @example
+  #
+  #   howl = Howl.new
+  #   index = howl.add(:get, "/:id"){}
+  #   index.name = :index
+  #   howl.path(:index, :id => 1) #=> "/1"
+  #   howl.path(:index, :id => 2, :foo => "bar") #=> "/1?foo=bar"
+  #
+  # @return [String] return a generated path.
+  #
   def path(name, *args)
     params = args.delete_at(args.last.is_a?(Hash) ? -1 : 0) || {}
     saved_args = args.dup
