c3a2b077482f70c5fb45bc42737118a93454702e
namusyaka <namusyaka@gmail.com>
2013-08-25 21:19:21 +0900

    Refactor padrino instance methods.

--- lib/howl-router/padrino/ext/instance_methods.rb
+++ lib/howl-router/padrino/ext/instance_methods.rb
@@ -3,41 +3,44 @@ class Howl
   module Padrino
     module InstanceMethods
       private
+        def invoke_route(route, params, options = {})
+          original_params, parent_layout, successful = @params.dup, @layout, false
+          captured_params = params[:captures].is_a?(Array) ? params.delete(:captures) :
+                                                             params.values_at(*route.matcher.names.dup)
+
+          @route = request.route_obj = route
+          @params.merge!(params) if params.is_a?(Hash)
+          @params.merge!(:captures => captured_params) unless captured_params.empty?
+          @block_params = params
+
+          filter! :before if options[:first]
+
+          catch(:pass) do
+            begin
+              (route.before_filters - settings.filters[:before]).each{|block| instance_eval(&block) }
+              @layout = route.use_layout if route.use_layout
+              route.custom_conditions.each {|block|
+                pass if block.bind(self).call == false
+              } unless route.custom_conditions.empty?
+              halt_response = catch(:halt){ route_eval{ route.block[self, captured_params] }}
+              successful    = true
+              halt(halt_response)
+            ensure
+              (route.after_filters - settings.filters[:after]).each {|block| instance_eval(&block) } if successful
+              @layout, @params = parent_layout, original_params
+            end
+          end
+        end
+
         def route!(base = settings, pass_block = nil)
           Thread.current['padrino.instance'] = self
           code, headers, routes = base.compiled_router.call(@request.env)
 
           status(code)
           if code == 200
-            routes.each_with_index do |route_pair, index|
-              route = route_pair[0]
+            routes.each_with_index do |(route, howl_params), index|
               next if route.user_agent && !(route.user_agent =~ @request.user_agent)
-              original_params, parent_layout, successful = @params.dup, @layout, false
-
-              howl_params     = route_pair[1]
-              param_names     = route.matcher.names.dup
-              captured_params = howl_params[:captures].is_a?(Array) ? howl_params.delete(:captures) : howl_params.values_at(*param_names)
-
-              @route = request.route_obj = route
-              @params.merge!(howl_params) if howl_params.is_a?(Hash)
-              @params.merge!(:captures => captured_params) unless captured_params.empty?
-              @block_params = howl_params
-
-              filter! :before if index == 0
-
-              catch(:pass) do
-                begin
-                  (route.before_filters - settings.filters[:before]).each{|block| instance_eval(&block) }
-                  @layout = route.use_layout if route.use_layout
-                  route.custom_conditions.each {|block| pass if block.bind(self).call == false } unless route.custom_conditions.empty?
-                  halt_response = catch(:halt){ route_eval{ route.block[self, captured_params] }}
-                  successful    = true
-                  halt(halt_response)
-                ensure
-                   (route.after_filters - settings.filters[:after]).each {|block| instance_eval(&block) } if successful
-                   @layout, @params = parent_layout, original_params
-                end
-              end
+              invoke_route(route, howl_params, :first => index.zero?)
             end
           else
             route_eval do
