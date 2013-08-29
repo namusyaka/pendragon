7c78c85c0d4acf9a3eedbf06cad523a8efa7991f
namusyaka <namusyaka@gmail.com>
2013-08-30 06:33:18 +0900

    Add some comments.

--- lib/howl-router/matcher.rb
+++ lib/howl-router/matcher.rb
@@ -2,16 +2,39 @@ require 'mustermann'
 
 class Howl
   class Matcher
+    # @param [String] path The path is string or regexp.
+    # @option options [Hash] :capture Set capture for path pattern.
+    # @option options [Hash] :default_values Set default_values for path pattern.
+    #
+    # @return [Howl::Matcher]
+    #
     def initialize(path, options = {})
       @path           = path.is_a?(String) && path.empty? ? "/" : path
       @capture        = options.delete(:capture)
       @default_values = options.delete(:default_values)
     end
 
+    # Do the matching.
+    #
+    # @param [String] pattern The pattern is actual path (path_info etc).
+    #
+    # @return [MatchData] If the pattern matched this route, return a MatchData.
+    # @return [Nil] If the pattern doesn't matched this route, return a nil.
+    #
     def match(pattern)
       handler.match(pattern)
     end
 
+    # Expand the path with params.
+    #
+    # @param [Hash] params The params for path pattern.
+    #
+    # @example
+    #   matcher = Howl::Matcher.new("/foo/:bar")
+    #   matcher.expand(:bar => 123) #=> "/foo/123"
+    #   matcher.expand(:bar => "bar", :baz => "test") #=> "/foo/bar?baz=test"
+    #
+    # @return [String] A expaneded path.
     def expand(params)
       params = params.dup
       query = params.keys.inject({}) do |result, key|
@@ -24,10 +47,13 @@ class Howl
       expanded_path
     end
 
+    # @return [Boolean] This matcher's handler is mustermann ?
     def mustermann?
       handler.class == Mustermann::Rails
     end
 
+    # @return [Mustermann::Rails] Return a Mustermann::Rails when @path is string.
+    # @return [Regexp] Return a regexp when @path is regexp.
     def handler
       @handler ||= case @path
       when String
@@ -37,10 +63,12 @@ class Howl
       end
     end
 
+    # @return [String] Return a converted handler.
     def to_s
       handler.to_s
     end
 
+    # @return [Array] Return a named captures.
     def names
       mustermann? ? handler.names.map(&:to_sym) : []
     end
