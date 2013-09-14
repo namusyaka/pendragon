5ba56115e906fdbc3f4de0fdedd108bdabda5410
namusyaka <namusyaka@gmail.com>
2013-09-15 02:33:16 +0900

    Don't indent for private methods.

--- lib/howl-router/padrino/ext/class_methods.rb
+++ lib/howl-router/padrino/ext/class_methods.rb
@@ -51,129 +51,130 @@ class Howl
       end
 
       private
-        def route(verb, path, *args, &block)
-          options = case args.size
-            when 2
-              args.last.merge(:map => args.first)
-            when 1
-              map = args.shift if args.first.is_a?(String)
-              if args.first.is_a?(Hash)
-                map ? args.first.merge(:map => map) : args.first
-              else
-                {:map => map || args.first}
-              end
-            when 0
-              {}
-            else raise
-          end
 
-          route_options = options.dup
-          route_options[:provides] = @_provides if @_provides
-
-          if allow_disabled_csrf
-            unless route_options[:csrf_protection] == false
-              route_options[:csrf_protection] = true
+      def route(verb, path, *args, &block)
+        options = case args.size
+          when 2
+            args.last.merge(:map => args.first)
+          when 1
+            map = args.shift if args.first.is_a?(String)
+            if args.first.is_a?(Hash)
+              map ? args.first.merge(:map => map) : args.first
+            else
+              {:map => map || args.first}
             end
-          end
-
-          path, *route_options[:with] = path if path.is_a?(Array)
-          action = path
-          path, name, route_parents, options, route_options = *parse_route(path, route_options, verb)
-          options.reverse_merge!(@_conditions) if @_conditions
-
-          method_name = "#{verb} #{path}"
-          unbound_method = generate_method(method_name, &block)
-
-          block = block.arity != 0 ?
-            proc {|a,p| unbound_method.bind(a).call(*p) } :
-            proc {|a,p| unbound_method.bind(a).call }
-
-          invoke_hook(:route_added, verb, path, block)
-
-          # Howl route construction
-          path[0, 0] = "/" if path == "(.:format)"
-          route = router.add(verb.downcase.to_sym, path, route_options)
-          route.name = name if name
-          route.action = action
-          priority_name = options.delete(:priority) || :normal
-          priority = ROUTE_PRIORITY[priority_name] or raise("Priority #{priority_name} not recognized, try #{ROUTE_PRIORITY.keys.join(', ')}")
-          route.cache = options.key?(:cache) ? options.delete(:cache) : @_cache
-          route.parent = route_parents ? (route_parents.count == 1 ? route_parents.first : route_parents) : route_parents
-          route.host = options.delete(:host) if options.key?(:host)
-          route.user_agent = options.delete(:agent) if options.key?(:agent)
-          if options.key?(:default_values)
-            defaults = options.delete(:default_values)
-            route.default_values = defaults if defaults
-          end
-          options.delete_if do |option, captures|
-            if route.significant_variable_names.include?(option)
-              route.capture[option] = Array(captures).first
-              true
-            end
-          end
-
-          # Add Sinatra conditions
-          options.each {|o, a| route.respond_to?("#{o}=") ? route.send("#{o}=", a) : send(o, *a) }
-          conditions, @conditions = @conditions, []
-          route.custom_conditions.concat(conditions)
-
-          invoke_hook(:padrino_route_added, route, verb, path, args, options, block)
-
-          # Add Application defaults
-          route.before_filters.concat(@filters[:before])
-          route.after_filters.concat(@filters[:after])
-          if @_controller
-            route.use_layout = @layout
-            route.controller = Array(@_controller)[0].to_s
-          end
-
-          deferred_routes[priority] << [route, block]
-
-          route
+          when 0
+            {}
+          else raise
         end
 
-        def provides(*types)
-          @_use_format = true
-          condition do
-            mime_types = types.map {|t| mime_type(t) }.compact
-            url_format = params[:format].to_sym if params[:format]
-            accepts = request.accept.map {|a| a.to_str }
+        route_options = options.dup
+        route_options[:provides] = @_provides if @_provides
 
-            # per rfc2616-sec14:
-            # Assume */* if no ACCEPT header is given.
-            catch_all = (accepts.delete "*/*" || accepts.empty?)
-            matching_types = accepts.empty? ? mime_types.slice(0,1) : (accepts & mime_types)
-            if matching_types.empty? && types.include?(:any)
-              matching_types = accepts
-            end
-
-            if !url_format && matching_types.first
-              type = ::Rack::Mime::MIME_TYPES.find {|k, v| v == matching_types.first }[0].sub(/\./,'').to_sym
-              accept_format = CONTENT_TYPE_ALIASES[type] || type
-            elsif catch_all && !types.include?(:any)
-              type = types.first
-              accept_format = CONTENT_TYPE_ALIASES[type] || type
-            end
-
-            matched_format = types.include?(:any) ||
-                             types.include?(accept_format) ||
-                             types.include?(url_format) ||
-                             ((!url_format) && request.accept.empty? && types.include?(:html))
-            # per rfc2616-sec14:
-            # answer with 406 if accept is given but types to not match any
-            # provided type
-            halt 406 if
-              (!url_format && !accepts.empty? && !matched_format) ||
-              (settings.respond_to?(:treat_format_as_accept) && settings.treat_format_as_accept && url_format && !matched_format)
-
-            if matched_format
-              @_content_type = url_format || accept_format || :html
-              content_type(@_content_type, :charset => 'utf-8')
-            end
-
-            matched_format
+        if allow_disabled_csrf
+          unless route_options[:csrf_protection] == false
+            route_options[:csrf_protection] = true
           end
         end
+
+        path, *route_options[:with] = path if path.is_a?(Array)
+        action = path
+        path, name, route_parents, options, route_options = *parse_route(path, route_options, verb)
+        options.reverse_merge!(@_conditions) if @_conditions
+
+        method_name = "#{verb} #{path}"
+        unbound_method = generate_method(method_name, &block)
+
+        block = block.arity != 0 ?
+          proc {|a,p| unbound_method.bind(a).call(*p) } :
+          proc {|a,p| unbound_method.bind(a).call }
+
+        invoke_hook(:route_added, verb, path, block)
+
+        # Howl route construction
+        path[0, 0] = "/" if path == "(.:format)"
+        route = router.add(verb.downcase.to_sym, path, route_options)
+        route.name = name if name
+        route.action = action
+        priority_name = options.delete(:priority) || :normal
+        priority = ROUTE_PRIORITY[priority_name] or raise("Priority #{priority_name} not recognized, try #{ROUTE_PRIORITY.keys.join(', ')}")
+        route.cache = options.key?(:cache) ? options.delete(:cache) : @_cache
+        route.parent = route_parents ? (route_parents.count == 1 ? route_parents.first : route_parents) : route_parents
+        route.host = options.delete(:host) if options.key?(:host)
+        route.user_agent = options.delete(:agent) if options.key?(:agent)
+        if options.key?(:default_values)
+          defaults = options.delete(:default_values)
+          route.default_values = defaults if defaults
+        end
+        options.delete_if do |option, captures|
+          if route.significant_variable_names.include?(option)
+            route.capture[option] = Array(captures).first
+            true
+          end
+        end
+
+        # Add Sinatra conditions
+        options.each {|o, a| route.respond_to?("#{o}=") ? route.send("#{o}=", a) : send(o, *a) }
+        conditions, @conditions = @conditions, []
+        route.custom_conditions.concat(conditions)
+
+        invoke_hook(:padrino_route_added, route, verb, path, args, options, block)
+
+        # Add Application defaults
+        route.before_filters.concat(@filters[:before])
+        route.after_filters.concat(@filters[:after])
+        if @_controller
+          route.use_layout = @layout
+          route.controller = Array(@_controller)[0].to_s
+        end
+
+        deferred_routes[priority] << [route, block]
+
+        route
+      end
+
+      def provides(*types)
+        @_use_format = true
+        condition do
+          mime_types = types.map {|t| mime_type(t) }.compact
+          url_format = params[:format].to_sym if params[:format]
+          accepts = request.accept.map {|a| a.to_str }
+
+          # per rfc2616-sec14:
+          # Assume */* if no ACCEPT header is given.
+          catch_all = (accepts.delete "*/*" || accepts.empty?)
+          matching_types = accepts.empty? ? mime_types.slice(0,1) : (accepts & mime_types)
+          if matching_types.empty? && types.include?(:any)
+            matching_types = accepts
+          end
+
+          if !url_format && matching_types.first
+            type = ::Rack::Mime::MIME_TYPES.find {|k, v| v == matching_types.first }[0].sub(/\./,'').to_sym
+            accept_format = CONTENT_TYPE_ALIASES[type] || type
+          elsif catch_all && !types.include?(:any)
+            type = types.first
+            accept_format = CONTENT_TYPE_ALIASES[type] || type
+          end
+
+          matched_format = types.include?(:any) ||
+                           types.include?(accept_format) ||
+                           types.include?(url_format) ||
+                           ((!url_format) && request.accept.empty? && types.include?(:html))
+          # per rfc2616-sec14:
+          # answer with 406 if accept is given but types to not match any
+          # provided type
+          halt 406 if
+            (!url_format && !accepts.empty? && !matched_format) ||
+            (settings.respond_to?(:treat_format_as_accept) && settings.treat_format_as_accept && url_format && !matched_format)
+
+          if matched_format
+            @_content_type = url_format || accept_format || :html
+            content_type(@_content_type, :charset => 'utf-8')
+          end
+
+          matched_format
+        end
+      end
     end
   end
 end
--- lib/howl-router/padrino/ext/instance_methods.rb
+++ lib/howl-router/padrino/ext/instance_methods.rb
@@ -3,62 +3,63 @@ class Howl
   module Padrino
     module InstanceMethods
       private
-        def invoke_route(route, params, options = {})
-          original_params, parent_layout, successful = @params.dup, @layout, false
-          captured_params = params[:captures].is_a?(Array) ? params.delete(:captures) :
-                                                             params.values_at(*route.matcher.names.dup)
 
-          @_response_buffer = nil
-          @route = request.route_obj = route
-          @params.merge!(params) if params.is_a?(Hash)
-          #@params.merge!(:captures => captured_params) unless captured_params.empty?
-          @params.merge!(:captures => captured_params) if !captured_params.empty? && route.path.is_a?(Regexp)
-          @block_params = params
+      def invoke_route(route, params, options = {})
+        original_params, parent_layout, successful = @params.dup, @layout, false
+        captured_params = params[:captures].is_a?(Array) ? params.delete(:captures) :
+                                                           params.values_at(*route.matcher.names.dup)
 
-          filter! :before if options[:first]
+        @_response_buffer = nil
+        @route = request.route_obj = route
+        @params.merge!(params) if params.is_a?(Hash)
+        #@params.merge!(:captures => captured_params) unless captured_params.empty?
+        @params.merge!(:captures => captured_params) if !captured_params.empty? && route.path.is_a?(Regexp)
+        @block_params = params
 
-          catch(:pass) do
-            begin
-              (route.before_filters - settings.filters[:before]).each{|block| instance_eval(&block) }
-              @layout = route.use_layout if route.use_layout
-              route.custom_conditions.each {|block| pass if block.bind(self).call == false }
-              halt_response     = catch(:halt){ route_eval{ route.block[self, captured_params] }}
-              @_response_buffer = halt_response.is_a?(Array) ? halt_response.last : halt_response
-              successful        = true
-              halt(halt_response)
-            ensure
-              (route.after_filters - settings.filters[:after]).each {|block| instance_eval(&block) } if successful
-              @layout, @params = parent_layout, original_params
-            end
+        filter! :before if options[:first]
+
+        catch(:pass) do
+          begin
+            (route.before_filters - settings.filters[:before]).each{|block| instance_eval(&block) }
+            @layout = route.use_layout if route.use_layout
+            route.custom_conditions.each {|block| pass if block.bind(self).call == false }
+            halt_response     = catch(:halt){ route_eval{ route.block[self, captured_params] }}
+            @_response_buffer = halt_response.is_a?(Array) ? halt_response.last : halt_response
+            successful        = true
+            halt(halt_response)
+          ensure
+            (route.after_filters - settings.filters[:after]).each {|block| instance_eval(&block) } if successful
+            @layout, @params = parent_layout, original_params
+          end
+        end
+      end
+
+      def route!(base = settings, pass_block = nil)
+        Thread.current['padrino.instance'] = self
+        code, headers, routes = base.compiled_router.call(@request.env)
+
+        status(code)
+        if code == 200
+          routes.each_with_index do |(route, howl_params), index|
+            next if route.user_agent && !(route.user_agent =~ @request.user_agent)
+            invoke_route(route, howl_params, :first => index.zero?)
+          end
+        else
+          route_eval do
+            headers.each{|k, v| response[k] = v } unless headers.empty?
+            route_missing if code == 404
+            route_missing if allow = response['Allow'] and allow.include?(request.env['REQUEST_METHOD'])
           end
         end
 
-        def route!(base = settings, pass_block = nil)
-          Thread.current['padrino.instance'] = self
-          code, headers, routes = base.compiled_router.call(@request.env)
-
-          status(code)
-          if code == 200
-            routes.each_with_index do |(route, howl_params), index|
-              next if route.user_agent && !(route.user_agent =~ @request.user_agent)
-              invoke_route(route, howl_params, :first => index.zero?)
-            end
-          else
-            route_eval do
-              headers.each{|k, v| response[k] = v } unless headers.empty?
-              route_missing if code == 404
-              route_missing if allow = response['Allow'] and allow.include?(request.env['REQUEST_METHOD'])
-            end
-          end
-
-          if base.superclass.respond_to?(:router)
-            route!(base.superclass, pass_block)
-            return
-          end
-
-          route_eval(&pass_block) if pass_block
-          route_missing
+        if base.superclass.respond_to?(:router)
+          route!(base.superclass, pass_block)
+          return
         end
+
+        route_eval(&pass_block) if pass_block
+        route_missing
+      end
     end
   end
 end
