c7f43a8a4156ddf7613f548a74d11d6e887e8e75
namusyaka <namusyaka@gmail.com>
2014-02-15 11:44:55 +0900

    Introduce Configuration class

--- lib/howl-router.rb
+++ lib/howl-router.rb
@@ -1,14 +1,36 @@
 require 'howl-router/router'
 
 module Howl
-  ##
+
   # Allow the verbs of these.
   HTTP_VERBS = [:get, :post, :delete, :put, :head]
 
-  ##
-  # A new instance of Howl::Router
-  # @see Howl::Router#initialize
-  def self.new(&block)
-    Router.new(&block)
+  class << self
+    # A new instance of Howl::Router
+    # @see Howl::Router#initialize
+    def new(&block)
+      Router.new(&block)
+    end
+  
+    # Yields Howl configuration block
+    # @example
+    #   Howl.configure do |config|
+    #     config.enable_compiler = true
+    #   end
+    # @see Howl::Configuration
+    def configure(&block)
+      block.call(configuration) if block_given?
+      configuration
+    end
+  
+    # Returns Howl configuration
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    # Resets Howl configuration
+    def reset_configuration!
+      @configuration = nil
+    end
   end
 end
---
+++ lib/howl-router/configuration.rb
@@ -0,0 +1,25 @@
+module Howl
+  class Configuration
+
+    # Enables to compile the routes
+    # Improve the performance by using this option,
+    # but some features will not work correctly.
+    # @see Howl::Router#compile
+    attr_accessor :enable_compiler
+
+    # Constructs an instance of Howl::Configuration
+    def initialize
+      @enable_compiler = false
+    end
+
+    # Returns an instance variable
+    def [](variable_name)
+      instance_variable_get("@#{variable_name}")
+    end
+
+    # Returns a boolean of @enable_compiler
+    def enable_compiler?
+      !!@enable_compiler
+    end
+  end
+end
