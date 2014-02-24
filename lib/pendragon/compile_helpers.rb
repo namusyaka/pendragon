7ed7b45d31ab71b9acbb760a6b2bcbef280d6096
namusyaka <namusyaka@gmail.com>
2014-02-25 08:30:58 +0900

    Fix some features that don't work when using compiler mode

--- lib/pendragon/compile_helpers.rb
+++ lib/pendragon/compile_helpers.rb
@@ -3,29 +3,45 @@ module Pendragon
   module CompileHelpers
     def compile!
       return if compiled?
-      @compiled_regexps = Pendragon::HTTP_VERBS.inject({}){|all, verb| all[verb] = []; all }
-      @routes.each_with_index do |route, index|
+      @regexps = @routes.map.with_index do |route, index|
         regexp = route.matcher.handler
         regexp = regexp.to_regexp if route.matcher.mustermann?
-        @compiled_regexps[route.verb] << /(?<_#{index}>#{regexp})/
-        route.index = "_#{index}"
+        route.index = index
+        /(?<_#{index}>#{regexp})/
       end
-      @compiled_regexps.each_pair{|verb, regexps| @compiled_regexps[verb] = /\A#{Regexp.union(regexps)}\Z/ }
+      @regexps = compile(@regexps)
+    end
+
+    def compile(regexps, paths = [])
+      return paths if regexps.length.zero?
+      paths << Regexp.union(regexps)
+      regexps.shift
+      compile(regexps, paths)
     end
 
     def compiled?
-      !!@compiled_regexps
+      !!@regexps
     end
 
     def recognize_by_compiling_regexp(request)
       pattern, verb, params = parse_request(request)
-      unless @compiled_regexps[verb] === pattern
-        old_pattern = pattern
-        pattern = pattern[0..-2] if pattern != "/" and pattern.end_with?("/")
-        raise NotFound if old_pattern == pattern || !(@compiled_regexps[verb] === pattern)
+      candidacies = match_with(pattern)
+      raise_exception(404) if candidacies.empty?
+      candidacies.reject!{|route| route.verb != verb }
+      raise_exception(405) if candidacies.empty?
+      candidacies.map{|route| [route, params_for(route, pattern, params)]}
+    end
+
+    def match_with(pattern)
+      offset = 0
+      conditions = [pattern]
+      conditions << pattern[0..-2] if pattern != "/" && pattern.end_with?("/")
+      loop.with_object([]) do |_, candidacies|
+        return candidacies unless conditions.any?{|x| @regexps[offset] === x }
+        route = @routes[offset..-1].detect{|route| Regexp.last_match("_#{route.index}") }
+        candidacies << route
+        offset = route.index + 1
       end
-      route = @routes.select{|route| route.verb == verb }.detect{|route| Regexp.last_match(route.index) }
-      [[route, params_for(route, pattern, params)]]
     end
   end
 end
