0a258f46fd53399c8ebbbdf7fb204cb172c49244
namusyaka <namusyaka@gmail.com>
2016-11-26 04:10:54 +0900

    drop r2ree

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ gem 'rack-test'
 gem 'test-unit'
 gem 'mocha'
 gem 'sinatra'
-gem 'r2ree'
 gem 'yard'
 gem 'modulla'
 gemspec
--- test/router/test_radix.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path('../helper', __dir__)
-
-class TestLiner < Test::Unit::TestCase
-  include Supports::SharedExamplesForRouting
-  router_class Pendragon::Radix
-end
